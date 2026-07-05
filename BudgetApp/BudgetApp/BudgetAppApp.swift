18ab07ac42cc9c30cff2986efc31028eb82d8832
Ajay <kumarraj312000@gmail.com>
2026-07-05 18:24:40 +0530

    added core data stack

---
+++ BudgetApp/BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Ajay kumar on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetApp/BudgetApp/Provider/CoreDataServices.swift
@@ -0,0 +1,37 @@
+//
+//  CoreDataServices.swift
+//  BudgetApp
+//
+//  Created by Ajay kumar on 05/07/26.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataServices {
+    
+    let persistContainer: NSPersistentContainer
+    
+    private var viewContext: NSManagedObjectContext {
+        return persistContainer.viewContext
+    }
+    
+    
+    init(isMemory: Bool = false) {
+        
+        persistContainer = NSPersistentContainer(name: "BudgetAppModel")
+        
+        if isMemory {
+            persistContainer.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        persistContainer.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Core Data store failed to be initialize \(error.localizedDescription)")
+            }
+        }
+        
+        
+    }
+    
+}
